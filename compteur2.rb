66c223f5ffed926053fa389eb9602f6fb319a5e7
FabienMllts <fabienmallet@gmail.com>
2018-01-15 17:22:49 +0100

    version 2 compteur.rb

--- compteur2.rb
+++ compteur2.rb
@@ -1,15 +1,27 @@
+def jean_michel_data(corpus, dictionary)
 
-def compter()
-	puts "enter your text: "
-	text = gets.chomp
+	occurences = Hash.new(0) 
 
-	words = text.split(" ")
-	frequencies = Hash.new(0)
-	words.each { |words| frequencies[words] +=1 }
-	frequencies = frequencies.sort_by { |words, frequencies| }
-	frequencies.reverse!
-	frequencies.each { |words, frequencies|
-		puts words + " " + frequencies.to_s 
-	}
+	#corpus : mettre tout en downcase et faire un split
+	corpus_words = corpus.downcase.split(' ')
+
+	#naviguer dans le dictionnaire
+	dictionary.each do |dictionary_word|
+		
+		#naviger dans le corpus
+		corpus_words.each do |corpus_word|
+	
+			#increment le hash si le mot est présent dans le dictionnaire
+			occurences[dictionary_word] += 1 if corpus_word.include? dictionary_word
+
+		end
+
+	end
+
+	return occurences
 end
-puts compter()
+
+corpus = "Howdy partner, sit down! How's it going?"
+
+dictionary = ["below", "down", "go", "going", "horn", "how", "howdy", "it", "i", "low", "own", "part", "partner", "sit"]	
+puts jean_michel_data(corpus, dictionary)
